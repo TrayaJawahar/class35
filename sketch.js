5b3aaa8f4bdf8fc4ccd82cc51ecb958f4d630a9a
TrayaJawahar <72916948+TrayaJawahar@users.noreply.github.com>
2021-01-26 13:56:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database,
+    gameState=0,
+    playercount=0;
+var form , player, game;
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+game = new Game();
+game.getState();
+game.start();
+ 
+}
+
+function draw(){
+  background("white");
+  
+   
+    drawSprites();
+  
+}
+
